46c3ad0b1e8b51a9650961185e1fc273ffb23d03
zzfn <chenc@fengyuntec.com>
2020-09-26 16:33:20 +0800

    :ambulance:

--- script/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-export NODE_OPTIONS=--max-old-space-size=8192
-npm i
-npm run build
-tar -zcvf build.tar.gz ./build
--- script/lint.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-npm run prettier
-npm run lint
-npm run stylelint --config .stylelintrc.js
