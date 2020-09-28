13816466c16c1bdf5da324e7f44b0af35c951b0f
chenc <chenc@fengyuntec.com>
2020-09-28 11:26:09 +0800

    chore: 升级依赖

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,5 @@
 const baseUrl = (): string => {
   switch (process.env.NODE_ENV) {
-    case 'development':
-      return 'https://api.zzfzzf.com';
     case 'test':
       return 'https://api.zzfzzf.com';
     default:
--- src/utils/sum.ts
+++
@@ -1,8 +0,0 @@
-function sum(a: number, b: number): number {
-  if (a && b) {
-    return a + b;
-  } else {
-    return a + b;
-  }
-}
-export default sum;
