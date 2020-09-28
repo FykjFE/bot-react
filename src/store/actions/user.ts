3717c156d82bcce437999991c4f38f88385bd147
chenc <chenc@fengyuntec.com>
2020-09-28 13:01:16 +0800

    chore: 升级依赖

--- src/store/actions/user.ts
+++ src/store/actions/user.ts
@@ -2,10 +2,6 @@ import { SET_USER_INFO, SET_USER_STATUS, User } from 'store/constants/user';
 import { GET_USER_INFO } from 'services/user.service';
 import { Dispatch } from 'redux';
 
-export function setUser(user: User): BaseAction<User> {
-  return { type: SET_USER_INFO, payload: user };
-}
-
 export function login(): BaseAction<User> {
   return { type: SET_USER_STATUS, payload: { isLogin: true } };
 }
