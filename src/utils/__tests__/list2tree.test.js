0d3fe413b6f8e10be7b8cea0b3e8f4c8eb8508e7
chenc <chenc@fengyuntec.com>
2020-09-28 17:12:49 +0800

    chore: 测试单元测试

--- src/utils/__tests__/list2tree.test.js
+++ src/utils/__tests__/list2tree.test.js
@@ -1,4 +1,5 @@
-import list2tree from '../src/utils/list2tree';
+import React from 'react';
+import list2tree from '../list2tree';
 
 const list = [
   { id: '12', parentId: '0', text: 'Man', level: '1' },
@@ -14,8 +15,8 @@ const tree = [
     text: 'Man',
     level: '1',
     children: [
-      { id: '6', parentId: '12', text: 'Boy', level: '2', children: null },
-      { id: '7', parentId: '12', text: 'Other', level: '2', children: null },
+      { id: '6', parentId: '12', text: 'Boy', level: '2' },
+      { id: '7', parentId: '12', text: 'Other', level: '2' },
     ],
   },
   {
@@ -23,9 +24,11 @@ const tree = [
     parentId: '0',
     text: 'Woman',
     level: '1',
-    children: [{ id: '11', parentId: '9', text: 'Girl', level: '2', children: null }],
+    children: [{ id: '11', parentId: '9', text: 'Girl', level: '2' }],
   },
 ];
-test('list2tree', () => {
-  expect(list2tree(list, { pid: 'parentId' })).toBe(tree);
+describe('测试数组转树工具方法', () => {
+  it('执行数组转树list2tree', () => {
+    expect(list2tree(list, { pid: 'parentId' })).toEqual(tree);
+  });
 });
