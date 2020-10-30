a6f56107b972f50d6b8953784615b587c688090f
chenc <chenc@fengyuntec.com>
2020-10-30 14:36:40 +0800

    chore: 移除redux

--- src/store/reducers/index.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import userReducer from './userReducer';
-import routeReducer from './routeReducer';
-
-export const rootReducer = combineReducers({
-  route: routeReducer,
-  user: userReducer,
-});
-export type RootState = ReturnType<typeof rootReducer>;
